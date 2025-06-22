ddf2229da99a61498b042b39382da9e07740ae17
FvCastr0 <fvdc2008@gmail.com>
2025-06-22 20:48:20 -0300

    Creating menu hamburger and animating

--- frontend/src/components/ui/navbar/index.tsx
+++ frontend/src/components/ui/navbar/index.tsx
@@ -1,51 +1,47 @@
 "use client";
 
 import { theme } from "@/components/shared/theme";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { BsCart2 } from "react-icons/bs";
 import { CiHeart, CiSearch } from "react-icons/ci";
-import { IoPersonOutline } from "react-icons/io5";
+import { IoCloseSharp, IoPersonOutline } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 import styled from "styled-components";
 
 const NavStyled = styled.nav`
+  position: sticky;
+  top: 0;
   display: flex;
   align-items: center;
-  margin: ${theme.spacing.lg};
+  padding: ${theme.spacing.lg};
   justify-content: space-evenly;
+  backdrop-filter: blur(8px);
+  box-shadow: ${theme.shadows.md};
 
-  ul {
-    display: flex;
-    gap: ${theme.spacing.lg};
-    align-items: center;
+  @media (max-width: 860px) {
+    justify-content: space-between;
+  }
 
-    a {
-      font-family: Inter;
-      color: ${theme.colors.textSecondary};
-      text-decoration: none;
-      transition: 0.3s all ease-in;
+  #menu {
+    display: none;
+    font-size: 2rem;
+    cursor: pointer;
 
-      &:hover {
-        color: ${theme.colors.black};
-        text-decoration: underline;
-      }
-      &:first-child {
-        color: ${theme.colors.black};
-      }
-    }
-
-    div {
-      display: flex;
-      gap: ${theme.spacing.md};
-      a {
-        color: ${theme.colors.black};
-      }
+    @media (max-width: 860px) {
+      display: block;
     }
   }
 `;
 
 const SearchButton = styled.div`
   display: flex;
+
+  @media (max-width: 860px) {
+    display: none;
+  }
 
   input {
     padding: ${theme.spacing.md} ${theme.spacing.lg};
@@ -66,10 +62,85 @@ const SearchButton = styled.div`
   }
 `;
 
+const NavLinks = styled(motion.div)<{ isOpen: boolean }>`
+  display: flex;
+  gap: ${theme.spacing.lg};
+  align-items: center;
+  a {
+    font-family: Inter;
+    color: ${theme.colors.textSecondary};
+    text-decoration: none;
+    transition: 0.3s all ease-in;
+
+    &:hover {
+      color: ${theme.colors.black};
+      text-decoration: underline;
+    }
+    &:first-child {
+      color: ${theme.colors.black};
+    }
+  }
+
+  div {
+    display: flex;
+    gap: ${theme.spacing.md};
+    a {
+      color: ${theme.colors.black};
+    }
+  }
+
+  @media (max-width: 860px) {
+    display: none;
+    @media (max-width: 860px) {
+      display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+      flex-direction: column;
+      position: absolute;
+      top: 96px;
+      right: 22px;
+      background: ${theme.colors.background};
+      padding: ${theme.spacing.lg};
+      border-radius: ${theme.radii.md};
+      box-shadow: ${theme.shadows.md};
+      gap: ${theme.spacing.md};
+      z-index: 10;
+      transition: all ease-in 0.3s;
+
+      a {
+        font-family: Inter;
+        color: ${theme.colors.textSecondary};
+        text-decoration: none;
+
+        &:hover {
+          color: ${theme.colors.black};
+          text-decoration: underline;
+        }
+      }
+
+      div {
+        display: flex;
+        gap: ${theme.spacing.md};
+
+        a {
+          color: ${theme.colors.black};
+        }
+      }
+    }
+  }
+`;
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <NavStyled>
       <Image src={"/images/logo.png"} alt="logo" width={96} height={29} />
+
+      {!isOpen ? (
+        <RxHamburgerMenu onClick={toggleMenu} id="menu" />
+      ) : (
+        <IoCloseSharp onClick={toggleMenu} id="menu" />
+      )}
 
       <SearchButton>
         <label htmlFor="search">
@@ -77,24 +148,34 @@ export default function Navbar() {
         </label>
         <input type="search" id="search" placeholder="Search" />
       </SearchButton>
+      <AnimatePresence>
+        {isOpen && (
+          <NavLinks
+            key="navLinks"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.1, ease: "easeIn" }}
+            isOpen={isOpen}
+          >
+            <Link href={"/"}>Home</Link>
+            <Link href={"/"}>About</Link>
+            <Link href={"/"}>Contact us</Link>
 
-      <ul>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/"}>About</Link>
-        <Link href={"/"}>Contact us</Link>
-
-        <div>
-          <Link href={"/"}>
-            <CiHeart size={24} />
-          </Link>
-          <Link href={"/"}>
-            <BsCart2 size={24} />
-          </Link>
-          <Link href={"/"}>
-            <IoPersonOutline size={24} />
-          </Link>
-        </div>
-      </ul>
+            <div>
+              <Link href={"/"}>
+                <CiHeart size={24} />
+              </Link>
+              <Link href={"/"}>
+                <BsCart2 size={24} />
+              </Link>
+              <Link href={"/"}>
+                <IoPersonOutline size={24} />
+              </Link>
+            </div>
+          </NavLinks>
+        )}
+      </AnimatePresence>
     </NavStyled>
   );
 }
